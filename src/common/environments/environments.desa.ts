9d449e8ad99dbc0e2f67c617bee8b4e38aa6837a
FABRICA <luis.farid@outlook.com>
2024-06-20 14:41:49 -0600

    peticion con https

--- src/common/environments/environments.desa.ts
+++ src/common/environments/environments.desa.ts
@@ -3,6 +3,6 @@
 export const environment = {
     production: false,
     // basePathUrl: 'http://localhost:8090',
-    basePathUrl: 'http://52.49.162.42:8090',
+    basePathUrl: 'https://52.49.162.42:8090',
  
 }
