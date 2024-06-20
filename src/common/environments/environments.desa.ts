87739e78fea974fa3d34c14c49dd53cb9e9181de
FABRICA <luis.farid@outlook.com>
2024-06-20 14:38:11 -0600

    ip peticiones configuracion

--- src/common/environments/environments.desa.ts
+++ src/common/environments/environments.desa.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
     production: false,
-    basePathUrl: 'http://localhost:8090',
-    // basePathUrl: 'https://5sdp2cdf-8090.usw3.devtunnels.ms',
+    // basePathUrl: 'http://localhost:8090',
+    basePathUrl: 'http://52.49.162.42:8090',
  
 }
