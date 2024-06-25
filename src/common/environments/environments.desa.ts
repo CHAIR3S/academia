ce6a703b8b23d5bcb917729baef8f86911369d33
FABRICA <luis.farid@outlook.com>
2024-06-25 13:15:58 -0600

    conectado a backend de railway va muy rapido

--- src/common/environments/environments.desa.ts
+++ src/common/environments/environments.desa.ts
@@ -3,6 +3,7 @@
 export const environment = {
     production: false,
     // basePathUrl: 'http://localhost:8090',
-    basePathUrl: 'https://academia-service-y30v.onrender.com',
+    // basePathUrl: 'https://academia-service-y30v.onrender.com',
+    basePathUrl: 'https://academia-service-production.up.railway.app'
  
 }
